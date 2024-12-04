8e79599f1e2aed64110bdd467b27e2d28b31b507
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-12-04 18:03:50 +0100

    Node redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -9,7 +9,7 @@ client.on('connect', () => console.log('Redis client connected to the server'));
 client.subscribe('holberton school channel');
 
 client.on('message', (channel, message) => {
-    console.log(message);
+    if (channel === 'holberton school channel') console.log(message);
 
     if (message === 'KILL_SERVER') {
         client.unsubscribe('holberton school channel');
